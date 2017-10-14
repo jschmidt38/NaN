ef12b9235725168ee60055c74edab1375cad83be
Paul <pg4hamill@gmail.com>
2017-10-14 14:52:53 -0400

    fix all changes

--- webapp/index.js
+++ webapp/index.js
@@ -1,20 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-'use strict';
+"use strict";
 
-var ipc = require('electron').ipcRenderer;
-var remote = require('remote');
-var Tray = remote.require('tray');
-var Menu = remote.require('menu');
-var path = require('path');
+var ipc = require("electron").ipcRenderer;
+var path = require("path");
 
-var login = document.querySelector('button[id=login]');
-if(login){
 
-    console.log('linked');
-}
-login.addEventListener('click', function () {
-    ipc.send('load-login');
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.querySelector("#ping")
+        .addEventListener("click", function () {
+        ipc.send("load-login");
+    });
 });
-
