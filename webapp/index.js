1730213ea225090baf92e59b2887c9834ea74aa0
coaltman <coaltergeist@gmail.com>
2017-10-14 15:21:15 -0400

    ui updates

--- webapp/index.js
+++ webapp/index.js
@@ -10,7 +10,7 @@ var path = require("path");
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    document.querySelector("#ping")
+    document.querySelector("#login")
         .addEventListener("click", function () {
         ipc.send("load-login");
     });
