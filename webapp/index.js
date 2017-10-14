901b54752d60c0cd7db79d0a84b33c0fac2ad24d
Nathaniel Rabinovich <Cody78n@gmail.com>
2017-10-14 15:05:32 -0400

    pinging now working

--- webapp/index.js
+++ webapp/index.js
@@ -9,11 +9,27 @@ var path = require('path');
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    var login = document.querySelector('#ping');
+    var login = document.querySelector("#login");
     
-    login.addEventListener('click', function () {
+    login.addEventListener("click", function () {
         console.log("dank");
-        ipc.send('load-login');
+        ipc.send("load-login");
     });
     console.log("loaded");
+});
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var ping = document.querySelector("#ping");
+
+	ping.addEventListener("click", function(event,arg) {
+		console.log("clicked");
+		ipc.send("pingtest", "204.2.229.9");
+	});
+	console.log("loaded");
+});
+
+ipc.on("pingtest-reply", (event, arg) => {
+	console.log(arg);
+
 });
