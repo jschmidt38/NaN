9f0fe466f8319ab2458482d303c42225de110aad
John Schmidt <jschmidt38@gatech.edu>
2017-10-14 16:21:16 -0400

    push cause I forgot

--- webapp/index.js
+++ webapp/index.js
@@ -2,16 +2,31 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 "use strict";
-
 var ipc = require("electron").ipcRenderer;
 var path = require("path");
 
 
-
+var key = "Ah-fxnT1s5WVvzbmH-OZNl7AeUF4pLpNMfgz4WYn5WOnH9cyQDJCKksgWvYNhmo-";
+var url = "http://dev.virtualearth.net/REST/v1/Imagery/Map";
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     document.querySelector("#ping")
         .addEventListener("click", function () {
         ipc.send("load-login");
     });
+
+    
+    loadMapScenario();
+    
+
+
+    
+
+
 });
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.querySelector("#myMap"), {
+        credentials: key
+    });
+}
