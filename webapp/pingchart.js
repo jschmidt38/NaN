407e71e68120007d4ce85292be219258bcd39b6d
coaltman <coaltergeist@gmail.com>
2017-10-14 21:46:30 -0400

    login on the 2nd page

--- webapp/pingchart.js
+++ webapp/pingchart.js
@@ -8,9 +8,10 @@ var url = "http://dev.virtualearth.net/REST/v1/Imagery/Map";
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 	
-    document.querySelector("#login")
+    document.querySelector("#truelogin")
         .addEventListener("click", function () {
         //ipc.send("load-login");
+        console.log("Login clicked");
 
     });
 
@@ -25,4 +26,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
         .addEventListener("click", function () {
         ipc.send("load-home");
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
 });
