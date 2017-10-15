06db4fbce4f7fa6c3c2c36bbb7ebb13e16efe7ee
Paul <pg4hamill@gmail.com>
2017-10-14 22:47:19 -0400

    finish /test/post route

--- server/postresults.go
+++ server/postresults.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -28,7 +29,6 @@ func postResultsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err != nil || failed || userToken == "" {
-
 		resp := &resultsResponse{Success: false,
 			Message: "error 400: bad request",
 		}
@@ -55,8 +55,49 @@ func postResultsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer db.Close()
-	dataCenter++
-	avgPing++
-	hopCount++
 
+	var (
+		userID          int
+		ispID           int
+		userIP          string
+		dataCenterVerif int
+		resp            *resultsResponse
+		b               []byte
+	)
+
+	db.QueryRow("SELECT id, ipAddr, ispID, FROM Users WHERE token = ?", userToken).Scan(&userID, &ispID, &userIP)
+	if userIP == "" || len(userIP) == 0 || userID <= 0 {
+		goto badRequest400
+	}
+	db.QueryRow("SELECT id FROM Datacenters WHERE id = ?", dataCenter).Scan(&dataCenterVerif)
+	if dataCenterVerif != dataCenter {
+		goto badRequest400
+	}
+
+	_, err = db.Exec(`INSERT INTO Results(userID, dataCenterID, testDate, ipAddr, avgPing, hopCount, ispID) 
+		VALUES(?, ?, GETDATE(), ?, ?, ?, ?)`, userID, dataCenter, userIP, avgPing, hopCount, ispID)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Panic(err.Error())
+	}
+	resp = &resultsResponse{Success: true}
+	b, err = json.Marshal(resp)
+	if err != nil {
+		panic("error marshalling in test")
+	}
+	w.Write(b)
+	return
+
+badRequest400:
+	resp = &resultsResponse{Success: false,
+		Message: "error 400: bad authorization",
+	}
+	b, err = json.Marshal(resp)
+	if err != nil {
+		panic("error marshalling in test")
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write(b)
+	return
 }
